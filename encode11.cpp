f41d57b8689cb8b0f0f28f901754764879cc7ff6
David <gek@katherine>
2021-03-08 02:30:16 -0500

    Automatic commit.

--- encode11.cpp
+++ encode11.cpp
@@ -4,18 +4,29 @@
 #include <cstdint>
 #include <cmath>
 
-//Idea: St
+//Idea: Serialize the 6 bits as normal, but then, 
+//Store information in the values between 96 and 126. (64-94 before encoding.)
+//We can store extra information into a character  if the high bit is 
+unsigned char encode_char(uint64_t& n){
+	unsigned char bottom6 = n & 63; //6 bits
+	unsigned char highbit = n & 64; //7th bit
+	n>>=6;
+	return bottom6 + '!';
+}
+uint64_t decode_char(char c){
+	return c - '!';
+}
 std::string serialize_num(uint64_t num){
 	char target[12] = {0};
 	for(int i = 0; i < 11; i++)
-		{target[i] = (num & 63) + ' ';num >>= 6;}
+		{target[i] = encode_char(num);}
 	return std::string(target);
 }
 
 uint64_t deserialize_num(std::string str){
 	uint64_t num = 0;
 	for(int i = 0; i < 11; i++)
-		{num |= (uint64_t)(str[i]-' ')<<((i)*6);}
+		{num |= decode_char(str[i])<<((i)*6);}
 	return num;
 }
 
