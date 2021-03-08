902a8c33ce340e8e41e57b684a85e420300f662d
David <gek@katherine>
2021-03-08 03:03:23 -0500

    Automatic commit.

--- encode11.cpp
+++ encode11.cpp
@@ -4,17 +4,34 @@
 #include <cstdint>
 #include <cmath>
 
-//Idea: Serialize the 6 bits as normal, but then, 
-//Store information in the values between 96 and 126. (64-94 before encoding.)
-//We can store extra information into a character  if the high bit is 
+//IDEA
+//use hamming code type techniques to store an extra bit of information
+//for each char.
+//
+inline unsigned char getnumbits(unsigned char c){
+	return (
+		((c&128) > 0) +
+		((c&64) > 0) +
+		((c&32) > 0) +
+		((c&16) > 0) +
+		((c&8) > 0) +
+		((c&4) > 0) +
+		((c&2) > 0) +
+		((c&1) > 0)
+	);
+}
 unsigned char encode_char(uint64_t& n){
 	unsigned char bottom6 = n & 63; //6 bits
 	unsigned char highbit = n & 64; //7th bit
 	n>>=6;
 	return bottom6 + '!';
 }
-uint64_t decode_char(char c){
-	return c - '!';
+uint64_t decode_char(unsigned char c){
+	uint64_t result;
+	c -= '!';
+	unsigned char bottom6 = c & 63; //6 bits
+	unsigned char highbit = getnumbits(bottom6)&1; //7th bit
+	return c;
 }
 std::string serialize_num(uint64_t num){
 	char target[12] = {0};
@@ -37,6 +54,8 @@ int main(int argc, char** argv){
 	if(argc > 1){
 		mystr = std::string(argv[1]);
 		n = strtoull(mystr.c_str(),0,10);
+		if(argc > 2)
+			n <<= strtoull(argv[2],0,10);
 	}
 	mystr = serialize_num(n);
 	uint64_t n_2 = deserialize_num(mystr);
